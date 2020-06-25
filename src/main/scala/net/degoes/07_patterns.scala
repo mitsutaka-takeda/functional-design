06b5812581d806663db5eebbf67feb63e283797f
Mitsutaka Takeda <mitsutaka.takeda@gmail.com>
2020-06-26 02:46:50 +0900

    Done chapter 7

--- src/main/scala/net/degoes/07_patterns.scala
+++ src/main/scala/net/degoes/07_patterns.scala
@@ -1,5 +1,9 @@
 package net.degoes
 
+import cats.Id
+
+import scala.util.Try
+
 /*
  * INTRODUCTION
  *
@@ -14,6 +18,7 @@ package net.degoes
  * BINARY COMPOSITION PATTERNS FOR VALUES - EXERCISE SET 1
  */
 object binary_values {
+
   object Exercise1 {
 
     /**
@@ -28,9 +33,9 @@ object binary_values {
      *
      * for all `a`, `b`, `c`.
      */
-    type SomeType
+    type SomeType = Int
 
-    def compose(left: SomeType, right: SomeType): SomeType = ???
+    def compose(left: SomeType, right: SomeType): SomeType = left + right
   }
 
   object Exercise2 {
@@ -47,9 +52,9 @@ object binary_values {
      *
      * for all `a`, `b`, `c`.
      */
-    type SomeType
+    type SomeType = String
 
-    def compose(left: SomeType, right: SomeType): SomeType = ???
+    def compose(left: SomeType, right: SomeType): SomeType = left + right
   }
 
   object Exercise3 {
@@ -66,9 +71,9 @@ object binary_values {
      *
      * for all `a`, `b`.
      */
-    type SomeType
+    type SomeType = Int
 
-    def compose(left: SomeType, right: SomeType): SomeType = ???
+    def compose(left: SomeType, right: SomeType): SomeType = left * right
   }
 
   object Exercise4 {
@@ -85,9 +90,9 @@ object binary_values {
      *
      * for all `a`, `b`.
      */
-    type SomeType
+    type SomeType = Set[Int]
 
-    def compose(left: SomeType, right: SomeType): SomeType = ???
+    def compose(left: SomeType, right: SomeType): SomeType = left.union(right)
   }
 
   object Exercise5 {
@@ -101,9 +106,15 @@ object binary_values {
      * combine two queries into one query, such that both results would
      * be queried when the model is executed.
      */
-    type SomeType
+    sealed trait Query
 
-    def compose(left: SomeType, right: SomeType): SomeType = ???
+    object Query {
+
+      final case class Both(l: Query, r: Query) extends Query
+
+    }
+
+    def compose(left: Query, right: Query): Query = Query.Both(left, right)
   }
 
   object Exercise6 {
@@ -114,9 +125,15 @@ object binary_values {
      * Choose or create a different type such that your implementation
      * of `compose` represents modeling "both".
      */
-    type SomeType
+    sealed trait Filter
 
-    def compose(left: SomeType, right: SomeType): SomeType = ???
+    object Filter {
+
+      final case class Both(f1: Filter, f2: Filter) extends Filter
+
+      def compose(left: Filter, right: Filter): Filter = Both(left, right)
+    }
+
   }
 
   object Exercise7 {
@@ -129,9 +146,16 @@ object binary_values {
      * a data type that represents a query, then this `compose` could
      * model running one query, but if it fails, running another.
      */
-    type SomeType
+    sealed trait Query
 
-    def compose(left: SomeType, right: SomeType): SomeType = ???
+    object Query {
+
+      final case class Or(lhs: Query, rhs: Query) extends Query
+
+
+      def compose(left: Query, right: Query): Query = Or(left, right)
+    }
+
   }
 
   object Exercise8 {
@@ -142,9 +166,15 @@ object binary_values {
      * Choose or create a different type such that your implementation
      * of `compose` represents modeling "or".
      */
-    type SomeType
+    sealed trait Filter
 
-    def compose(left: SomeType, right: SomeType): SomeType = ???
+    object Filter {
+
+      final case class Or(lhs: Filter, rhs: Filter) extends Filter
+
+      def compose(left: Filter, right: Filter): Filter = Or(left, right)
+    }
+
   }
 
   object Exercise9 {
@@ -161,11 +191,11 @@ object binary_values {
      *
      * for all `a`.
      */
-    type SomeType
+    type SomeType = Int
 
-    def identity: SomeType = ???
+    def identity: SomeType = 0
 
-    def compose(left: SomeType, right: SomeType): SomeType = ???
+    def compose(left: SomeType, right: SomeType): SomeType = left + right
   }
 
   object Exercise10 {
@@ -182,18 +212,20 @@ object binary_values {
      *
      * for all `a`.
      */
-    type SomeType
+    type SomeType = String
 
-    def identity: SomeType = ???
+    def identity: SomeType = ""
 
-    def compose(left: SomeType, right: SomeType): SomeType = ???
+    def compose(left: SomeType, right: SomeType): SomeType = left + right
   }
+
 }
 
 /**
  * BINARY COMPOSITION PATTERNS FOR TYPE CONSTRUCTORS - EXERCISE SET 2
  */
 object binary_tcs {
+
   object Exercise1 {
 
     /**
@@ -208,9 +240,13 @@ object binary_tcs {
      *
      * for all `a`, `b`, `c`, where `~` means "equivalent to".
      */
-    type SomeType[A]
+    type SomeType[A] = Option[A]
 
-    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[(A, B)] = ???
+    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[(A, B)] =
+      for {
+        a <- left
+        b <- right
+      } yield (a, b)
   }
 
   object Exercise2 {
@@ -227,9 +263,12 @@ object binary_tcs {
      *
      * for all `a`, `b`, `c`, where `~` means "equivalent to".
      */
-    type SomeType[A]
+    type SomeType[A] = Try[A]
 
-    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[(A, B)] = ???
+    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[(A, B)] = for {
+      a <- left
+      b <- right
+    } yield (a, b)
   }
 
   object Exercise3 {
@@ -265,9 +304,12 @@ object binary_tcs {
      *
      * for all `a`, `b`, `c`, where `~` means "equivalent to".
      */
-    type SomeType[A]
+    type SomeType[A] = Option[A]
 
-    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[Either[A, B]] = ???
+    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[Either[A, B]] = left match {
+      case Some(value) => Some(Left(value))
+      case None => right.map(Right(_))
+    }
   }
 
   object Exercise5 {
@@ -284,9 +326,9 @@ object binary_tcs {
      *
      * for all `a`, `b`, where `~` means "equivalent to".
      */
-    type SomeType[A]
+    type SomeType[A] = Id[A]
 
-    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[(A, B)] = ???
+    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[(A, B)] = (left, right)
   }
 
   object Exercise6 {
@@ -303,9 +345,12 @@ object binary_tcs {
      *
      * for all `a`, `b`, where `~` means "equivalent to".
      */
-    type SomeType[A]
+    type SomeType[A] = Option[A]
 
-    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[(A, B)] = ???
+    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[(A, B)] = for {
+      x <- left
+      y <- right
+    } yield (x, y)
   }
 
   object Exercise7 {
@@ -322,9 +367,10 @@ object binary_tcs {
      *
      * for all `a`, `b`, where `~` means "equivalent to".
      */
-    type SomeType[A]
+    type SomeType[A] = Set[A]
 
-    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[Either[A, B]] = ???
+    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[Either[A, B]] =
+      left.map(Left(_): Either[A, B]).union(right.map(Right(_): Either[A, B]))
   }
 
   object Exercise8 {
@@ -341,9 +387,10 @@ object binary_tcs {
      *
      * for all `a`, `b`, where `~` means "equivalent to".
      */
-    type SomeType[A]
+    type SomeType[A] = List[A]
 
-    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[Either[A, B]] = ???
+    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[Either[A, B]] =
+      left.map(Left(_): Either[A, B]) ++ right.map(Right(_): Either[A, B])
   }
 
   object Exercise9 {
@@ -357,9 +404,15 @@ object binary_tcs {
      * combine two queries into one query, such that both results would
      * be queried when the model is executed.
      */
-    type SomeType[A]
+    sealed trait Query[A]
 
-    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[(A, B)] = ???
+    object Query {
+
+      final case class Both[A, B](q1: Query[A], q2: Query[B]) extends Query[(A, B)]
+
+      def compose[A, B](left: Query[A], right: Query[B]): Query[(A, B)] = Both(left, right)
+    }
+
   }
 
   object Exercise10 {
@@ -370,9 +423,15 @@ object binary_tcs {
      * Choose or create a different type such that your implementation
      * of `compose` represents modeling "both".
      */
-    type SomeType[A]
+    sealed trait ComputedValue[A]
 
-    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[(A, B)] = ???
+    object ComputedValue {
+
+      final case class Both[A, B](left: ComputedValue[A], right: ComputedValue[B]) extends ComputedValue[(A, B)]
+
+      def compose[A, B](left: ComputedValue[A], right: ComputedValue[B]): ComputedValue[(A, B)] = Both(left, right)
+    }
+
   }
 
   object Exercise11 {
@@ -385,9 +444,15 @@ object binary_tcs {
      * a data type that represents a query, then this `compose` could
      * model running one query, but if it fails, running another.
      */
-    type SomeType[A]
+    sealed trait Query[A]
 
-    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[Either[A, B]] = ???
+    object Query {
+
+      final case class Or[A, B](first: Query[A], second: Query[B]) extends Query[Either[A, B]]
+
+      def compose[A, B](left: Query[A], right: Query[B]): Query[Either[A, B]] = Or(left, right)
+    }
+
   }
 
   object Exercise12 {
@@ -398,9 +463,15 @@ object binary_tcs {
      * Choose or create a different type such that your implementation
      * of `compose` represents modeling "or".
      */
-    type SomeType[A]
+    sealed trait Filter[A]
 
-    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[Either[A, B]] = ???
+    object Filter {
+
+      final case class Or[A, B](first: Filter[A], second: Filter[B]) extends Filter[Either[A, B]]
+
+      def compose[A, B](left: Filter[A], right: Filter[B]): Filter[Either[A, B]] = Or(left, right)
+    }
+
   }
 
   object Exercise13 {
@@ -417,11 +488,14 @@ object binary_tcs {
      *
      * for all `a`, where `~` means "equivalent to".
      */
-    type SomeType[A]
+    type SomeType[A] = Option[A]
 
-    def identity: SomeType[Any] = ???
+    def identity: SomeType[Unit] = Some(())
 
-    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[(A, B)] = ???
+    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[(A, B)] = for {
+      l <- left
+      r <- right
+    } yield (l, r)
   }
 
   object Exercise14 {
@@ -441,11 +515,12 @@ object binary_tcs {
      * Note that `Either[A, Nothing]` is equivalent to `A`, and
      * `Either[Nothing, A]` is equivalent to `A`.
      */
-    type SomeType[A]
+    type SomeType[A] = Option[A]
 
     def identity: SomeType[Nothing] = ???
 
-    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[Either[A, B]] = ???
+    def compose[A, B](left: SomeType[A], right: SomeType[B]): SomeType[Either[A, B]]
+    = left.map(Left(_)).orElse(right.map(Right(_)))
   }
 
 }
@@ -454,6 +529,7 @@ object binary_tcs {
  * IMPERATIVE PATTERNS FOR VALUES - EXERCISE SET 3
  */
 object imperative_values {
+
   trait Exercise1 {
 
     /**
@@ -461,10 +537,15 @@ object imperative_values {
      *
      * Choose or create a data type such that you can implement `andThen` in
      * such a way that it models sequential composition.
+     *
+     * sequential composition ?
      */
-    type SomeType
+    type SomeType = Int
 
-    def andThen(first: SomeType, second: SomeType): SomeType = ???
+    /**
+     * Is this sequential composition?
+     */
+    def andThen(first: SomeType, second: SomeType): SomeType = first + second
   }
 
   trait Exercise2 {
@@ -475,16 +556,21 @@ object imperative_values {
      * Choose or create a different type such that you can implement `andThen` in
      * such a way that it models sequential composition.
      */
-    type SomeType
+    type SomeType = Either[String, Int]
 
-    def andThen(first: SomeType, second: SomeType): SomeType
+    def andThen(first: SomeType, second: SomeType): SomeType = for {
+      f <- first
+      s <- second
+    } yield f + s
   }
+
 }
 
 /**
  * IMPERATIVE PATTERNS FOR TYPE CONSTRUCTORS - EXERCISE SET 4
  */
 object imperative_tcs {
+
   trait Exercise1 {
 
     /**
@@ -493,9 +579,9 @@ object imperative_tcs {
      * Choose or create a data type such that you can implement `andThen` in
      * such a way that it models sequential composition.
      */
-    type SomeType[A]
+    type SomeType[A] = Either[String, A]
 
-    def andThen[A, B](first: SomeType[A], second: A => SomeType[B]): SomeType[B] = ???
+    def andThen[A, B](first: SomeType[A], second: A => SomeType[B]): SomeType[B] = first.flatMap(second)
   }
 
   trait Exercise2 {
@@ -506,39 +592,53 @@ object imperative_tcs {
      * Choose or create a different type such that you can implement `andThen` in
      * such a way that it models sequential composition.
      */
-    type SomeType[A]
+    type SomeType[A] = Try[A]
 
-    def andThen[A, B](first: SomeType[A], second: A => SomeType[B]): SomeType[B]
+    def andThen[A, B](first: SomeType[A], second: A => SomeType[B]): SomeType[B] = first.flatMap(second)
   }
+
 }
 
 /**
  * RECIPES - GRADUATION PROJECT
  */
 object recipes {
+
   sealed trait Baked[+A]
+
   object Baked {
-    final case class Burnt[A](value: A)         extends Baked[A]
+
+    final case class Burnt[A](value: A) extends Baked[A]
+
     final case class CookedPerfect[A](value: A) extends Baked[A]
-    final case class Undercooked[A](value: A)   extends Baked[A]
+
+    final case class Undercooked[A](value: A) extends Baked[A]
+
   }
 
   sealed trait Ingredient
+
   object Ingredient {
-    final case class Eggs(number: Int)        extends Ingredient
-    final case class Sugar(amount: Double)    extends Ingredient
-    final case class Flour(amount: Double)    extends Ingredient
+
+    final case class Eggs(number: Int) extends Ingredient
+
+    final case class Sugar(amount: Double) extends Ingredient
+
+    final case class Flour(amount: Double) extends Ingredient
+
     final case class Cinnamon(amount: Double) extends Ingredient
+
   }
 
   sealed trait Recipe[+A] {
+    self =>
 
     /**
      * EXERCISE 1
      *
      * Implement a `map` operation that allows changing what a recipe produces.
      */
-    def map[B](f: A => B): Recipe[B] = ???
+    def map[B](f: A => B): Recipe[B] = Recipe.Map(self, f)
 
     /**
      * EXERCISE 2
@@ -546,7 +646,7 @@ object recipes {
      * Implement a `combine` operation that allows combining two recipes into
      * one, producing both items in a tuple.
      */
-    def combine[B](that: Recipe[B]): Recipe[(A, B)] = ???
+    def combine[B](that: Recipe[B]): Recipe[(A, B)] = Recipe.Both(self, that)
 
     /**
      * EXERCISE 3
@@ -554,7 +654,7 @@ object recipes {
      * Implement a `tryOrElse` operation that allows trying a backup recipe,
      * in case this recipe ends in disaster.
      */
-    def tryOrElse[B](that: Recipe[B]): Recipe[Either[A, B]] = ???
+    def tryOrElse[B](that: Recipe[B]): Recipe[Either[A, B]] = Recipe.TryOrElse(self, that)
 
     /**
      * EXERCISE 4
@@ -565,18 +665,32 @@ object recipes {
      * NOTE: Be sure to update the `make` method below so that you can make
      * recipes that use your new operation.
      */
-    def flatMap[B](f: A => Recipe[B]): Recipe[B] = ???
+    def flatMap[B](f: A => Recipe[B]): Recipe[B] = Recipe.FlatMap(self, f)
   }
+
   object Recipe {
-    case object Disaster                                              extends Recipe[Nothing]
-    final case class AddIngredient(ingredient: Ingredient)            extends Recipe[Ingredient]
+
+    case object Disaster extends Recipe[Nothing]
+
+    final case class AddIngredient(ingredient: Ingredient) extends Recipe[Ingredient]
+
     final case class Bake[A](recipe: Recipe[A], temp: Int, time: Int) extends Recipe[Baked[A]]
+
+    final case class Map[A, B](recipe: Recipe[A], f: A => B) extends Recipe[B]
+
+    final case class Both[A, B](r1: Recipe[A], r2: Recipe[B]) extends Recipe[(A, B)]
+
+    final case class TryOrElse[A, B](recipe: Recipe[A], alternative: Recipe[B]) extends Recipe[Either[A, B]]
+
+    final case class FlatMap[A, B](recipe: Recipe[A], f: A => Recipe[B]) extends Recipe[B]
+
   }
+
   import Recipe._
 
   def make[A](recipe: Recipe[A]): A =
     recipe match {
-      case Disaster                  => throw new Exception("Uh no, utter disaster!")
+      case Disaster => throw new Exception("Uh no, utter disaster!")
       case AddIngredient(ingredient) => println(s"Adding ${ingredient}"); ingredient
       case Bake(recipe, temp, time) =>
         val a = make(recipe)
@@ -586,5 +700,16 @@ object recipes {
         if (time * temp < 1000) Baked.Undercooked(a)
         else if (time * temp > 6000) Baked.Burnt(a)
         else Baked.CookedPerfect(a)
+      case Map(recipe, f) =>
+        f(make(recipe))
+      case Both(r1, r2) =>
+        make(r1) -> make(r2)
+      case Recipe.TryOrElse(recipe, alternative) =>
+        recipe match {
+          case Recipe.Disaster => Right(make(alternative))
+          case x => Left(make(x))
+        }
+      case FlatMap(recipe, f) =>
+        make(f(make(recipe)))
     }
 }
